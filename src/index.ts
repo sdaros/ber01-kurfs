e6fdc98e554511a5377b5acbe4fadc0b25ae41db
patman1987 <51786687+patman1987@users.noreply.github.com>
2022-01-06 07:07:31 +0000

    Deploying to gh-pages from master @ 934892f7848e096fa6093721537ed3e3dd4c1fd5 🚀

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,33 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
+// Pat´s Test
+// AUSGEBLENDET BY PAT
+/*
+// Testnew popup
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+
+/* helloWorldPopup = WA.room.onEnterLayer("WelcomeMessagePopup").subscribe(() => {
+    WA.ui.openPopup("WelcomeMessage", 'Welcome to our new Common Area!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("WelcomeMessage").subscribe(() => {
+    helloWorldPopup.close();
+})
+*/
+// Ab hier der orignale Teil
+
+// Original unterhalb
 let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
