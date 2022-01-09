98ed41da6cce506c71228732fdf7b7aef88296c0
Stefano Da Ros <sdaros@cip.li>
2022-01-09 19:48:52 +0100

    More zone updates

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,77 @@
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+// The function below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+async function extendedFeatures() {
+    try {
+        await bootstrapExtra()
+        console.log('Scripting API Extra loaded successfully');
+    } catch (error) {
+        console.error('Scripting API Extra ERROR',error);
+    }
+}
+extendedFeatures();
 
-let currentPopup: any = undefined;
+
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
+// Manage popups
+let currentZone: string;
+let currentPopup: any;
+const config = [
+    {
+        zone: 'clock',
+        message: "It's " + time,
+        cta: []
+    },
+    {
+        zone: 'loungeEntrance',
+        message: 'Welcome to our our new Office, BER01, perhaps it looks familiar to you.',
+        cta: [
+            {
+                label: 'Close',
+                className: 'normal',
+                callback: (popup: any) => popup.close,
+            },
+        ]
+    },
+    {
+        zone: 'owlyBase',
+        message: 'The OwlyBase is just one click away',
+        cta: [
+            {
+                label: 'Open OwlyBase',
+                className: 'normal',
+                callback: () => WA.nav.openTab('https://confluence.signavio.com/x/KodNB'),
+            },
+        ]
+    }
+];
+WA.room.onEnterZone('clock', () => openPopup('clock'));
+WA.room.onLeaveZone('clock', closePopup);
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-WA.room.onLeaveZone('clock', closePopUp);
+WA.room.onEnterZone('loungeEntrance', () => openPopup('loungeEntrance'));
+WA.room.onLeaveZone('loungeEntrance', closePopup);
 
-WA.room.onEnterZone('WelcomeMessagePopup', () => {
-    currentPopup =  WA.ui.openPopup("WelcomeMessage","Welcome to our new Common Area!",[{
-        label: "Close",
-        className: "normal",
-        callback: (popup) => {
-            popup.close();
-        }
-    }]);
-})
-WA.room.onLeaveZone('WelcomeMessagePopup', closePopUp);
+WA.room.onEnterZone('owlyBase', () => openPopup('owlyBase'));
+WA.room.onLeaveZone('owlyBase', closePopup);
+WA.room.onLeaveZone('OwlyBase', closePopup);
 
-WA.room.onEnterZone('OwlyBase', () => {
-    currentPopup =  WA.ui.openPopup("owlyBasePopup","OwlyBase is just one click away",[{
-        label: "Open Owly Base",
-        className: "normal",
-        callback: () => {
-            WA.nav.openTab('https://confluence.signavio.com/x/KodNB')
-        }
-    }]);
-})
-WA.room.onLeaveZone('OwlyBase', closePopUp);
+// Popup management functions
+function openPopup(zoneName: string) {
+    currentZone = zoneName
+    const popupName = zoneName + 'Popup'
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
 
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        // Close the popup when the "Close" button is pressed
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.ui.openPopup(popupName, zone.message, zone.cta)
+    }
+}
+function closePopup(){
+    if (typeof currentPopup !== 'undefined') {
         currentPopup.close();
         currentPopup = undefined;
     }
